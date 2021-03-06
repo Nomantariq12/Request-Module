1eab03e2120ed926cd493cab9a17372c71dcc20c
faizantariqalvi <faizantariqalvi@gmail.com>
2021-03-06 10:23:00 +0500

    Request Module first commit

---
+++ request_module.py
@@ -0,0 +1,11 @@
+import requests
+
+url = "https://api.github.com/users/sana-rasheed"
+req = requests.get(url)
+print(req)
+print(req.status_code)
+data = req.json()
+print("Following: ", data["following"])
+print("Followers: ", data["followers"])
+print("Public Repositories: ", data["public_repos"])
+print("The Owner of this repository is ", data["name"], "and The total number of public repositories are ", data["public_repos"])
